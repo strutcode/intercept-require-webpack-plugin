8882c281b4f834d707d1140c2c908c3732eafa14
Tristan Shelton <tristan.m.shelton@gmail.com>
2020-04-27 16:53:16 -0400

    Code cleanliness

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ export default class InterceptRequireWebpackPlugin {
 
   apply(compiler: Compiler) {
     const callback = this.callback
+    const unindent = (s: string) => s.replace(/^\s*/m, '')
 
     compiler.hooks.compilation.tap('InterceptRequirePlugin', function (
       compilation,
@@ -17,18 +18,21 @@ export default class InterceptRequireWebpackPlugin {
           const result = new ConcatSource()
           const context = moduleObject.context
 
-          result.add(`
-  if (typeof __webpack_require__ !== 'undefined') {
-    __webpack_require__ = (function (o) {
-      var cb = ${callback.toString()}
-      var i = function(q) {
-        return cb(q, ${JSON.stringify(context)}, o)
-      }
-      Object.assign(i, o);
-      return i;
-    })(__webpack_require__);
-  }
-  `)
+          result.add(
+            unindent(
+              `
+              if (typeof __webpack_require__ !== 'undefined') {
+                __webpack_require__ = (function (o) {
+                  var cb = ${callback.toString()}
+                  var i = function(q) {
+                    return cb(q, ${JSON.stringify(context)}, o)
+                  }
+                  Object.assign(i, o);
+                  return i;
+                })(__webpack_require__);
+              }`,
+            ),
+          )
 
           result.add(moduleSource)
 
